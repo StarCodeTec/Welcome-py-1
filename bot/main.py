e88839aedcf194e161ca6a020d6c11d258a4a9ca
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-21 08:52:32 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,6 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
-bot = commands.Bot(command_prefix='.', intents=intents)
+bot = commands.Bot(command_prefix='W^', intents=intents)
 
 bot.run("OTU1NDQwMjc5NDUwNzEwMDc2.YjhtGQ.kozZwra_R36aBqlq6PabGzgATVk")
