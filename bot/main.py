8b802cd164351acab75df594d0f3216f8fa4769e
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-21 11:42:31 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,13 +11,12 @@ async def on_raw_reaction_add(payload):
   general = bot.get_channel(955195564721573910)
   entrance = 955071525256568892
   member = discord.Member
-  print(member)
   user = bot.get_user(payload.user_id) 
   channel = bot.get_channel(payload.channel_id)
   msg = channel.get_partial_message(payload.message_id)
   emoji = str(payload.emoji)
   if payload.channel_id == entrance:
     if emoji == "":
-      await channel.send(user.mention)
+      print(member)
 
 bot.run("OTU1NDQwMjc5NDUwNzEwMDc2.YjhtGQ.kozZwra_R36aBqlq6PabGzgATVk")
