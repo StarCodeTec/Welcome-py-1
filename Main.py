dcdf4a2e8020020a3837da93b624a7d066eec204
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-21 07:56:00 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+import os
 import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+bot.run(os.environ['TOKEN'])
