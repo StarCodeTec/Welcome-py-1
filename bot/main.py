8e68125574e903e0a86f03e8e600af009be7cef3
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-21 09:54:49 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,4 +6,13 @@ intents.typing = False
 intents.presences = False
 bot = commands.Bot(command_prefix='W^ ', intents=intents)
 
+@bot.event
+async def on_raw_reaction_add(payload):
+  user = bot.get_user(payload.user_id) 
+  channel = bot.get_channel(payload.channel_id)
+  user.send(channel)
+  msg = channel.get_partial_message(payload.message_id)
+  emoji = str(payload.emoji)
+  
+
 bot.run("OTU1NDQwMjc5NDUwNzEwMDc2.YjhtGQ.kozZwra_R36aBqlq6PabGzgATVk")
