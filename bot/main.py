37226fe8227f02b005ee881bae7143aae23c09ea
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-22 10:42:46 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,15 +5,15 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 bot = commands.Bot(command_prefix='F^ ', intents=intents)
-general = bot.get_channel(923084022249320490) or await bot.fetch_channel(923084022249320490)
-entrance = bot.get_channel(955071525256568892) or await bot.fetch_channel(955071525256568892)
     
 @bot.command()
-async def gen_send(ctx, words):
-    await general.send(words)
+async def gen_send(ctx, words, user):
+    general = bot.get_channel(923084022249320490) or await bot.fetch_channel(923084022249320490)
+    await general.send(words,user)
 
 @bot.command()
 async def ent_send(ctx, words, user):
+    entrance = bot.get_channel(955071525256568892) or await bot.fetch_channel(955071525256568892)
     await entrance.send(words, user)
     
 
