0d17904280bee5eabf7a36c3cf5d9dbdbf9c09bb
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-22 07:54:54 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -23,6 +23,6 @@ async def on_raw_reaction_add(payload):
   msg = channel.get_partial_message(payload.message_id)
   emoji = str(payload.emoji)
   if emoji == "✅":
-    await test()
+    await test(user)
 
 bot.run("OTU1NDQwMjc5NDUwNzEwMDc2.YjhtGQ.kozZwra_R36aBqlq6PabGzgATVk")
