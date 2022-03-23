05d18504b47ade73d511519298db210e019fd3a4
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-23 06:20:15 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -18,9 +18,15 @@ async def check(ctx):
         print("false")
         return(False)
 @bot.command()
-async def gen_send(ctx, words, userid, sent=None):
-        general = bot.get_channel(950085161872154694) or await bot.fetch_channel(950085161872154694)
-        allwordg=f"<@!{userid}> {words}"
+async def gen_send(ctx, words, userid):
+        general = bot.get_channel(955517941812719687) or await bot.fetch_channel(955517941812719687)
+        if words == welcome_text:
+            member = ctx
+            admin_role = member.guild.get_role(955566126518136854)
+            if admin_role in member.roles:
+                allwordg=f"<@!{userid}> {words} sent by @{member}"
+        else:
+            allwordg=f"<@!{userid}> {words}"
 
         if userid == "none":
             await general.send(words)
@@ -43,20 +49,20 @@ async def on_raw_reaction_add(payload):
   user = bot.get_user(payload.user_id) 
   channel = bot.get_channel(payload.channel_id)
   msg = await channel.fetch_message(payload.message_id)
-  member = msg.author
+  member = paylaod.member
   auth = msg.author.id
   emoji = str(payload.emoji)
-  auth_role = payload.member.guild.get_role(955566126518136854)
+  auth_role = member.guild.get_role(955566126518136854)
   entrance = bot.get_channel(945087125831958588) or await bot.fetch_channel(945087125831958588)
   if auth_role in payload.member.roles:
       rolev = payload.member.guild.get_role(889011345712894002)
       roleu = payload.member.guild.get_role(889011029428801607)
       if channel == entrance:
           if emoji == "✅":
-              await payload.member.add_roles(rolev)
-              await payload.member.remove_roles(roleu)
+              await member.add_roles(rolev)
+              await member.remove_roles(roleu)
               await msg.delete()
-              await gen_send(user, welcome_text, auth, member)
+              await gen_send(member, welcome_text, auth)
 
 
 bot.run("OTU1NDQwMjc5NDUwNzEwMDc2.YjhtGQ.kozZwra_R36aBqlq6PabGzgATVk")
