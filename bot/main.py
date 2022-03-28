d573136714c98d146ed306a21d4e3848cf9a7074
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-28 09:05:18 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -12,7 +12,7 @@ welcome_text = "to the server. Feel free to make a bio and enjoy your stay!"
 Fenne = 474984052017987604 
 Equinox = 599059234134687774
 
-@tasks.loop(time=[datetime.time(hour=9, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=16, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=21, tzinfo=ZoneInfo("US/Eastern"))], count=None)
+@tasks.loop(time=[datetime.time(hour=9, minute=7, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=16, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=21, tzinfo=ZoneInfo("US/Eastern"))], count=None)
 async def gen():
         gen = bot.get_channel(950085161872154694) or await bot.fetch_channel(950085161872154694)
         gen.send("Remember everyone, please don't use profanity here!")
