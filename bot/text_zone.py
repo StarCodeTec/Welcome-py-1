814f575900255d593f3d04f61338b2c785be9a88
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-29 21:45:04 -0400

    Update text_zone.py

--- bot/text_zone.py
+++ bot/text_zone.py
@@ -6,12 +6,4 @@ welcome_text_dm = """to the Femboy Cafe!
 
 ⇀ Be sure to check in at the <#945087125831958588>  to verify for full access.
 ⇀ Share our server link discord.gg/femboycafe
-
-```⇀ Welcome to the Femboy Cafe!
-
-✧ₓ˚.✧ This server is owned and operated by Fenne Wolf
-✧ₓ˚.✧ Join me on Social Media!
-✧ₓ˚.✧ https://www.linktr.ee/littlefenne
-
-⇀ Be sure to check in at the <#945087125831958588> to verify for full access.
-⇀ Share our server link discord.gg/femboycafe```"""
+"""
