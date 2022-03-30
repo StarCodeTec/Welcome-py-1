092b0b7f552658e8a993d66ae7ca86d4fe12f2cf
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-03-30 11:17:35 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -19,7 +19,7 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('F^ ', 'F^'), inten
 welcome_text = "to the server. Feel free to make a bio and enjoy your stay!"
 Fenne = 474984052017987604 
 Equinox = 599059234134687774
-@tasks.loop(time=[datetime.time(hour=11, minute=15, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=18, tzinfo=ZoneInfo("US/Eastern"))], count=None)
+@tasks.loop(time=[datetime.time(hour=11, minute=21, tzinfo=ZoneInfo("US/Eastern")), datetime.time(hour=18, tzinfo=ZoneInfo("US/Eastern"))], count=None)
 async def bot():
         botxc = bot.get_channel(940377877214548008) or await bot.fetch_channel(940377877214548008)
         await botxc.send("Check out the pinned message to bump the server so more people can join!")
