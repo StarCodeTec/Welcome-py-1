d3b6bc161ce168736c180dc854072876751ce77c
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-12 11:20:53 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,7 @@ from text_zone import e2_re as check
 from text_zone import e3_re as r3
 from text_zone import e4_re as r4
 from text_zone import e5_re as r5
+from text_zone import bt as bio_template
 
 import os
 import discord
@@ -130,6 +131,11 @@ async def on_message(msg):
             await msg.add_reaction(r5)
         if msg.channel.id == 901207969922949161:
             await msg.add_reaction(check)
+        if msg.channel.id == 888482614351134720:
+            async for msg in msg.channel.history(limit=100):
+                if msg.author == bot.user:
+                    await msg.delete()
+                    await msg.send(bio_template)
         if msg.channel.category_id == 889022488720330816:
             if msg.channel.id != 889219939192410222:
                 await msg.add_reaction(r1)
