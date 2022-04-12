0419e2db72ab6833140834a07343c4b9b9c56623
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-12 11:21:17 -0400

    Update text_zone.py

--- bot/text_zone.py
+++ bot/text_zone.py
@@ -7,6 +7,8 @@ welcome_text_dm = """to the Femboy Cafe!
 ⇀ Be sure to check in at the <#945087125831958588>  to verify for full access.
 ⇀ Share our server link discord.gg/femboycafe
 """
+
+bt = """test"""
 e1_re = "<:little_fenne:925500399656509489>"
 e2_re = "<:check:919007866940182589>"
 e3_re = u"\U0001F49C"
