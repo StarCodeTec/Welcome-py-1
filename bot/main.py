8870d293a1b3794dbfa7a60c9f944fea53bcf243
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-13 07:39:02 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -161,7 +161,7 @@ async def on_raw_reaction_add(payload):
       roleu = payload.member.guild.get_role(889011029428801607)
 
       if channel == entrance:
-          if str(emoji) == check:
+          if str(emoji) == "<:check:919007866940182589>":
               await msg.author.add_roles(rolev)
               await msg.author.remove_roles(roleu)
               await msg.delete()
