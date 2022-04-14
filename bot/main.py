3ce45f082eb7fd940ab60637da6676663bb3c4c2
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-14 11:29:27 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -72,7 +72,6 @@ async def start(ctx):
     if ctx != "fhdfugdfvuvdjgduywdguwrggfuyfrufrkhdeiyutfu":
         c['bio'] = await biore.send(bio_template)
     if ctx == "fhdfugdfvuvdjgduywdguwrggfuyfrufrkhdeiyutfu":
-        c.delete()
         c['bio'] = await biore.send(bio_template)
 
 
