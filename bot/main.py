ac6169517c844cae3a316c3e831cac55124e4a8f
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-14 14:36:30 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -104,10 +104,10 @@ async def on_message(msg):
         elif msg.channel.id == 901207969922949161:
             await msg.add_reaction(rcheck)
         elif msg.channel.id == 888482614351134720:
-            def is_me(m):
+            def is_me(msg):
                 return m.author.id == botuser
             bioxtt = bot.get_channel(888482614351134720) or await bot.fetch_channel(888482614351134720)
-            await bioxtt.purge(limit=100, check=is_me(msg))
+            await bioxtt.purge(limit=100, check=is_me)
             await bioxtt.send(bio_template)
         elif msg.channel.category_id == 889022488720330816:
             if msg.channel.id != 889219939192410222:
