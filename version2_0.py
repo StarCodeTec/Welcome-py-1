bbe682463b5d68b471256d6216f7abc1b9ba9d65
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-28 18:11:21 -0400

    Update version2_0.py

--- version2_0.py
+++ version2_0.py
@@ -2,7 +2,11 @@ import discord
 from discord.ext import tasks, commands
 
 cog = commands.Cog
-class test(bot):
+class test(cog):
+  intents = discord.Intents.all()
+  intents.typing = False
+  intents.presences = False
+  bot = commands.Bot(command_prefix=commands.when_mentioned_or('F^ ', 'F^'), intents=intents)
   def __init__(self, bot):
     self.bot = bot
   
