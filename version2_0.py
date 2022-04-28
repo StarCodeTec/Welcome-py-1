decbc2acd401be66ead830d644c00ec27f350215
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-28 18:18:59 -0400

    Update version2_0.py

--- version2_0.py
+++ version2_0.py
@@ -3,11 +3,11 @@ from discord.ext import tasks, commands
 
 cog = commands.Cog
 class test(cog):
-  intents = discord.Intents.all()
-  intents.typing = False
-  intents.presences = False
-  bot = commands.Bot(command_prefix=commands.when_mentioned_or('F^ ', 'F^'), intents=intents)
   def __init__(self):
+    intents = discord.Intents.all()
+    intents.typing = False
+    intents.presences = False
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or('F^ ', 'F^'), intents=intents)
     self.bot = bot
   
   @commands.command()
