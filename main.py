94abb114582fe56af62a1fda72c72903b417aae0
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-28 18:47:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ r4 = id_0.P_heart
 r5 = id_0.thumb_up
 null = None
 botuser = 955440279450710076
-key = os.environ['API_ENDPOINT']
+key = os.environ['MAIN_KEY']
 #intents----------------------------------------------------------------------------------------
 
 intents = discord.Intents.all()
