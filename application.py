9b89dcebbf2af3f2f2d0de63ba2763ec3baa2a2a
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-28 14:02:33 -0400

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def run_script():
     file = open(r'./main.py', 'r').read()
     return exec(file)
