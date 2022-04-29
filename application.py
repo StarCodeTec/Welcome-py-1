a4320c57e2091a76e662e9795979bf705e5bcc1b
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-29 05:55:43 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from main import main
+from main import main_start
 import asyncio as a
 application = Flask(__name__)
 
 @application.route('/')
 def run_script():
-    return a.run(main())
+    return a.run(main_start())
 
 if __name__ == "__main__":
     application.run(debug=True)
