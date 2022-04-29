98135ef68004a3aff0d9236004c790040be99a0d
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-04-28 21:43:59 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 from flask import Flask
-
+from main import start_bot_true
 application = Flask(__name__)
 
 @application.route('/')
 def run_script():
-    file = open(r'./main.py', 'r').read()
-    return exec(file)
+    return start_bot_true
 
 if __name__ == "__main__":
     application.run(debug=True)
