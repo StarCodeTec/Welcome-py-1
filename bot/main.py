82a915a19cc01a468370418854a97f962551d416
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-03 11:14:00 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,45 @@
-import discord
-from discord.ext import tasks, commands
-botuser=955440279450710076
+#cogs--------------------------------------------------------------------------------------------  
 
-class Mainbot(commands.Cog):
-    def __init__(self, bot):
-      self.bot = bot
+#personal_import---------------------------------------------------------------------------------
+
+#import------------------------------------------------------------------------------------------
+import os
+import discord
+import time
+import traceback
+import asyncio
+from discord.ext import tasks, commands
+
+#EXTRA_VARS--------------------------------------------------------------------------------------
+key = os.environ['CUSTOM_ENV']
+null = None
+
+#intents----------------------------------------------------------------------------------------
+intents = discord.Intents.all()
+intents.typing = False
+intents.presences = False
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('F^ ', 'F^'), intents=intents)
+botuser = 955440279450710076
+#-----------------------------------------------------------------------------------------------
+
+@bot.event
+async def on_message(msg): 
+    auth = msg.author.id
+    if auth != botuser && msg.context == "test":
+        msg.channel.send("bot?")
     
-    @commands.Cog.listener()
-    async def on_message(self, msg):
-      channel=msg.channel.id
-      author=msg.author.id
-      if author==botuser:return
-      def add_react():
-            await msg.add_reaction(r1)
-            await msg.add_reaction(rcheck)
-            await msg.add_reaction(r3)            
-            await msg.add_reaction(r4)            
-            await msg.add_reaction(r5)
-      if 
+    
+    
+    
+    
+    
+    
+async def main_start():
+    async with bot:
+        #gen.start()
+        #bump.start()
+        #bystander.start()
+        #await bot.add_cog(test)
+        await bot.start(str(key)) 
+
+asyncio.run(main_start())
