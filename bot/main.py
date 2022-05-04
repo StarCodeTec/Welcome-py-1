15fe88187878172e90abe9c1cca3c9fb9d25c211
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-03 21:54:52 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -61,7 +61,8 @@ async def on_raw_reaction_add(payload):
   member = payload.member #Reacter
   gen = bot.get_channel(GEN) or await bot.fetch_channel(GEN)
   auth_role = member.guild.get_role(928077514411233350) or await member.guild.fetch_role(928077514411233350)
-  if auth_role in member.roles: 
+  auth = member.get_role(928077514411233350)
+  if auth in member.roles: 
       await gen.send(auth_role)
       await gen.send(member.roles)
       await gen.send("test")
