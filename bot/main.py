46bb7e78f782dcf23a1b9ff0a44f9bbd586e8aad
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-14 17:26:11 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -51,6 +51,9 @@ async def on_guild_channel_create(cha):
   if cha.category.id == cafe.cats.Home and "ticket" in str(cha.name):
     time.sleep(3)
     await cha.send("Hey there, how can we help you?")
+  elif msg.channel.id != cafe.Verify.Entrance:
+    if msg.channel.category_id != cafe.cats.Verify:return
+    cha.send("Please put your answers in one message!")
 
 @bot.event
 async def on_message(msg):
