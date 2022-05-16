daa7bc0f473e1e9f360cade19e316525ff4f03ce
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-16 13:47:58 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -69,14 +69,16 @@ async def on_message(msg):
     return msg.author.id == botuser
   if msg.channel.id == cafe.Little_fenne.News or msg.channel.category_id == cafe.cats.Selfies:
     if msg.channel.id == cafe.Selfies.Comments:return
-    rx1 = await msg.guild.fetch_emoji(r1)
+    rx1 = await msg.guild.fetch_emoji(925500399656509489)
+    rxcheck = await msg.guild.fetch_emoji(919007866940182589)
+
     await msg.add_reaction(rx1)
     await msg.add_reaction(rxcheck)
     await msg.add_reaction(r3)            
     await msg.add_reaction(r4)            
     await msg.add_reaction(r5)
   elif msg.channel.id == cafe.Mod.News:
-    rxcheck = await msg.guild.fetch_emoji(rcheck)
+    rxcheck = await msg.guild.fetch_emoji(919007866940182589)
     await msg.add_reaction(rxcheck)
   elif msg.channel.id == cafe.Chat.Promo:
     cha = bot.get_channel(cafe.Chat.Promo) or await bot.fetch_channel(cafe.Chat.Promo)
