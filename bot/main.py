bfaf599e37228951a6fe6bbb13b1f62e6a2bb664
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-17 07:51:11 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -102,8 +102,8 @@ async def on_message(msg):
       text = message.removesuffix("NONE")
     if msg.content.split()[-3] == "@":
       ping = msg.content.split()[-2]
-      del text.lsplit[-3]
-      del text.lsplit[-2]
+      del text.split()[-3]
+      del text.split()[-2]
       tex = ' '.join(text)
       await cha.send(f"{tex}<@{ping}>")
     else:
