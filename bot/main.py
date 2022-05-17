d873d583dbf20daa0deab59c7a0fee9ee7b25a66
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-16 20:32:02 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -92,7 +92,8 @@ async def on_message(msg):
     if msg.channel.id != 956295021676601386:return
     message = msg.content.removeprefix(".speak").lstrip()
     if message.endswith("GEN"):
-      print(f"{str(message).removesuffix("GEN")} in GEN")
+      text=message.removesuffix("GEN")
+      print(f"{text} in GEN")
     else:
       print(message)
     
