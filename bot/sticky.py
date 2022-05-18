02d81b0923c00a03483dd90ebc0690e6408e3dbd
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-18 12:23:40 -0400

    Update sticky.py

--- bot/sticky.py
+++ bot/sticky.py
@@ -16,15 +16,15 @@ class sticky(commands.Cog):
     def is_me(msg):
       return msg.author.id == botuser
     if msg.channel.id == cafe.Chat.Promo:
-      cha = bot.get_channel(cafe.Chat.Promo) or await bot.fetch_channel(cafe.Chat.Promo)
+      cha = self.bot.get_channel(cafe.Chat.Promo) or await self.bot.fetch_channel(cafe.Chat.Promo)
       await cha.purge(limit=2, check=is_me)
       await cha.send("Server boosters can post <#904501391299608586> in every 30 minutes!")
     elif msg.channel.id == cafe.Chat.Bio:
-      cha = bot.get_channel(cafe.Chat.Bio) or await bot.fetch_channel(cafe.Chat.Bio)   
+      cha = self.bot.get_channel(cafe.Chat.Bio) or await self.bot.fetch_channel(cafe.Chat.Bio)   
       await cha.purge(limit=2, check=is_me)
       await cha.send(b.bt)
     elif msg.channel.id == 973436882799177768:
-      cha = bot.get_channel(973436882799177768) or await bot.fetch_channel(973436882799177768)
+      cha = self.bot.get_channel(973436882799177768) or await self.bot.fetch_channel(973436882799177768)
       await cha.purge(limit=2, check=is_me)
       await cha.send("This is only for asking members to DM. Any advertising of your own DM's or friend requests may be removed and warned!")
   
