9259efa4eb80818f7d122d07eda349984f420505
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-17 23:40:34 -0400

    Create IDS.py

---
+++ bot/IDS.py
@@ -0,0 +1,59 @@
+class server:
+  cafe=
+  logs=
+class cafe:
+  class cats:
+    verify=
+    """AP does not need to read by bot"""
+  class verify:
+    ent=
+  class AP:
+    cmds=
+  class mod:
+    hq=
+    chat=
+    news=
+    busboy=
+    policy=
+    invites=
+    comamnds=
+    log=
+    logger=
+    strike=
+  class home:
+    about=
+    fenne=
+    insta=
+    announce=
+    reminders=
+    rules=
+    roles=
+    giveaways=
+    promo=
+    support_ticket=
+  class lgbtq:
+    polls=
+    announce=
+    tests=
+    resources=
+    
+  class ask_fenne:
+    
+  class agere:
+    
+  class chat:
+    
+  class selfies:
+    
+  class media:
+    
+  class friend_zone:
+    
+  class voice:
+    
+ 
+
+
+
+class logs:
+  
