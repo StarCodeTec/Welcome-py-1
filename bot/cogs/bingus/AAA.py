8e22422963e2535e551247a4a6fd0787ca99289e
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-26 13:13:45 -0400

    Update AAA.py

--- bot/cogs/bingus/AAA.py
+++ bot/cogs/bingus/AAA.py
@@ -1,5 +1,5 @@
 import random
-bingus= FILE()
 def FILE():
   file=["attachment://bingus.png", "attachment://bingus!.gif", "attachment://bingus-beloved.gif", "attachment://bingus-bongus.gif", "attachment://bingus-cat.gif", "attachment://bingus-cube.gif", "attachment://bingus-cult.gif", "attachment://bingus-cults.gif", "attachment://bingus-dance.gif", "attachment://bingus-dances.gif", "attachment://bingus-delilah.gif", "attachment://bingus-dimension.gif", "attachment://bingus-evolution.gif", "attachment://bingus-hater.gif", "attachment://bingus-haters.gif", "attachment://bingus-hi.gif", "attachment://bingus-love.gif", "attachment://bingus-loves.gif", "attachment://bingus-mar.gif", "attachment://bingus-meme.gif", "attachment://bingus-phone.gif", "attachment://bingus-tat.gif", "attachment://bingus.gif", "attachment://bingus_army.gif", "attachment://bingus_heart.gif", "attachment://bingus_smart.gif", "attachment://dead-chat.gif"]
   return random.choice(file)
+bingus=FILE()
