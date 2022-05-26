4fd1f1b1730b3217d2d4983ae17a962339c99570
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-26 12:52:59 -0400

    Update Acog.py

--- bot/cogs/bingus/Acog.py
+++ bot/cogs/bingus/Acog.py
@@ -2,14 +2,8 @@ import discord
 from discord.ext import tasks, commands
 from . import AAA as bingus
 cog = commands.Cog
-cafe = ID.cafe
 Fenne = 474984052017987604 
 Equinox = 599059234134687774
-r1 = id_0.fenne
-rcheck = id_0.check
-r3 = id_0.heart
-r4 = id_0.P_heart
-r5 = id_0.thumb_up
 null = None
 botuser = 966392608895152228 
 class bingus(commands.Cog):
