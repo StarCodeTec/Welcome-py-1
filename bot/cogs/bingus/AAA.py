594d7aa326d63a1cf209f61ad542884380eea504
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-27 11:21:57 -0400

    Delete AAA.py

--- bot/cogs/bingus/AAA.py
+++
@@ -1,5 +0,0 @@
-import random
-def FILE():
-  file=["bingus.png", "bingus!.gif", "bingus-beloved.gif", "bingus-bongus.gif", "bingus-cat.gif", "bingus-cube.gif", "bingus-cult.gif", "bingus-cults.gif", "bingus-dance.gif", "bingus-dances.gif", "bingus-delilah.gif", "bingus-dimension.gif", "bingus-evolution.gif", "bingus-hater.gif", "bingus-haters.gif", "bingus-hi.gif", "bingus-love.gif", "bingus-loves.gif", "bingus-mar.gif", "bingus-meme.gif", "bingus-phone.gif", "bingus-tat.gif", "bingus.gif", "bingus_army.gif", "bingus_heart.gif", "bingus_smart.gif", "dead-chat.gif"]
-  return str(random.choice(file))
-bingus=FILE()
