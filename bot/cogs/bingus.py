8b7684a5549baf83e31fd5c40c9ae0e0bacb0639
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-27 11:27:22 -0400

    Update bingus.py

--- bot/cogs/bingus.py
+++ bot/cogs/bingus.py
@@ -14,7 +14,7 @@ class bingus(commands.Cog):
   
   @commands.command()
   async def bingus(self, ctx):
-    file=["Z5EW9Ij", "apGRbbd", "bxSmibQ", "0pld30P", "ekv5sS", "cTQxzzz", "Qz0o2au", "0o0YOq9", "Cie89pF", "n4E8Eo7", "a52YHBu", "lYDqcIH", "TZ227yu", "T4RJ0mC", "UqnkEGP", "wAg3rsf", "wSWdCaT", "RqT1tFS", "3VJGRpY", "raerLvq", "LH8VqGH", "xtrV1fj", "GePK3z5", "qOKGQ9p", "dYt8wZk", "qvtC6Ix", "zvccvn8"]
+    file=["Z5EW9Ij", "apGRbbd", "bxSmibQ", "0pld30P", "ekv5sS", "cTQxzzz", "Qz0o2au", "0o0YOq9", "Cie89pF", "n4E8Eo7", "a52YHBu", "lYDqcIH", "TZ227yu", "T4RJ0mC", "UqnkEGP", "wAg3rsf", "wSWdCaT", "RqT1tFS", "3VJGRpY", "raerLvq", "LH8VqGH", "xtrV1fj", "GePK3z5", "qOKGQ9p", "dYt8wZk", "qvtC6Ix", "2vgNv4u"]
     files=random.choice(file)
     embed = discord.Embed()
     embed.set_image(url=f"https://i.imgur.com/{files}.gif")
