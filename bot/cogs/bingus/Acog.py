31b13b3d8216b39033af381c5d591e4d9a444ccf
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-05-26 22:44:13 -0400

    Update Acog.py

--- bot/cogs/bingus/Acog.py
+++ bot/cogs/bingus/Acog.py
@@ -17,5 +17,5 @@ class bingus(commands.Cog):
     files=["bingus!.gif", "bingus-beloved.gif", "bingus-bongus.gif", "bingus-cat.gif", "bingus-cube.gif", "bingus-cult.gif", "bingus-cults.gif", "bingus-dance.gif", "bingus-dances.gif", "bingus-delilah.gif", "bingus-dimension.gif", "bingus-evolution.gif", "bingus-hater.gif", "bingus-haters.gif", "bingus-hi.gif", "bingus-love.gif", "bingus-loves.gif", "bingus-mar.gif", "bingus-meme.gif", "bingus-phone.gif", "bingus-tat.gif", "bingus.gif", "bingus_army.gif", "bingus_heart.gif", "bingus_smart.gif", "dead-chat.gif"]
     filess=random.choice(files)
     embed = discord.Embed()
-    embed.set_image(url=f"https://i.imgur.com/zvccvn8.gif")
+    embed.set_image(url=f"https://i.imgur.com/{filess}.gif")
     await ctx.send(embed=embed)
