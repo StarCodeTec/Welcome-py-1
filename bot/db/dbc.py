c9a539a0ea90c4b5cf1c2c8573f6063c8380a62d
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-06-01 08:31:08 -0400

    Update dbc.py

--- bot/db/dbc.py
+++ bot/db/dbc.py
@@ -1,4 +1,3 @@
 from .. import passcodes
 password=passcodes.main.dbc
-client = pymongo.MongoClient(f"mongodb+srv://Jagg312:{password}@pybot00.74vkk.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
+client = f"mongodb+srv://Jagg312:{password}@pybot00.74vkk.mongodb.net/?retryWrites=true&w=majority"
