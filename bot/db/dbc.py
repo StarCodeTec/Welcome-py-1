8049ecb8a19bcdd83dc7c7d7f3017159481b8f8d
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-01 09:31:09 -0400

    update

--- bot/db/dbc.py
+++ bot/db/dbc.py
@@ -1,3 +1,5 @@
-from . import passcodes
+import sys
+sys.path.append("..")
+import passcodes
 password=passcodes.main.dbc
 client = f"mongodb+srv://Jagg312:{password}@pybot00.74vkk.mongodb.net/?retryWrites=true&w=majority"
