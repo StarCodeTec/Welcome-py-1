9893ae31a05061e033c844b4ccc10c8e2c85524c
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-06-03 09:17:13 -0400

    Update IDS.py

--- bot/extras/IDS.py
+++ bot/extras/IDS.py
@@ -5,8 +5,17 @@ class cafe:
   class cats:
     verify=
     """AP does not need to read by bot"""
-  class verify:
-    ent=
+    mod=
+    home=
+    lgbtq=
+    fenne=
+    little=
+    chat=
+    selfies=
+    media
+    friends=
+  #verify column 
+  verify=
   class AP:
     cmds=
   class mod:
@@ -21,39 +30,54 @@ class cafe:
     logger=
     strike=
   class home:
-    about=
     fenne=
+    about=
+    rules=
+    roles=
     insta=
     announce=
     reminders=
-    rules=
-    roles=
     giveaways=
     promo=
-    support_ticket=
+  support_ticket=
   class lgbtq:
-    polls=
     announce=
     tests=
     resources=
-    
-  class ask_fenne:
-    
-  class agere:
-    
+    happy= #vent
+    sad= #vent
+    polls=
+  outfit_advice=
+  open_support=
+  class fenne:
+    storys=
+  lgbtq_advice=
+  femboy_advice=
+  private_advice=
+  class little:
+    space=
+    media=
+    venting=
+    counsel=
+    nonverbal=
+    verify=
   class chat:
-    
+    gen=
+    tone=
+    vip=
+    dms=
+    suggestions=
+    bot=
+    bump= 
+    mod=   
   class selfies:
     
   class media:
     
-  class friend_zone:
+  class friends:
     
   class voice:
     
  
-
-
-
 class logs:
   
