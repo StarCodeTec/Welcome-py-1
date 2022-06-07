824b30d46b9dc78b3fc405bd71c35b495e23d315
acatia <69216256+acatiadroid@users.noreply.github.com>
2022-06-07 23:07:46 +0100

    Update bio iwhen bio msg is edited

--- bot/cogs/bio.py
+++ bot/cogs/bio.py
@@ -23,3 +23,19 @@ class bio(commands.Cog):
         view.add_item(discord.ui.Button(label="Go to bio post", url=msg.jump_url))
 
         await ctx.send(discord.utils.escape_mentions(f"**Bio for {member.name}**\n{data['bio']}"), view=view)
+    
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, payload):
+        if payload.channel_id != cafe.friends.bio:
+            return
+
+        channel = self.bot.get_channel(payload.channel_id)
+        msg = await channel.fetch_message(payload.message_id)
+
+        await self.bot.bio.upsert(
+          {
+            "_id": msg.author.id,
+            "bio": str(msg.content),
+            "msg_id": msg.id
+          }
+        )
