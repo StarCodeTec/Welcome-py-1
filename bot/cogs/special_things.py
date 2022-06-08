0fe83e3492a8334ef216b33bd43d6d03c7fb0cb7
jagg3127 <61505193+jagg3127@users.noreply.github.com>
2022-06-08 14:31:27 -0400

    Update special_things.py

--- bot/cogs/special_things.py
+++ bot/cogs/special_things.py
@@ -40,9 +40,11 @@ class special(commands.Cog):
 
     await channel.send(f"{message} {member.mention}" if member else message)
 
-
-  @cog.listener()
-  async def on_message(self, msg):
+  @commands.command(hidden=True)
+  async def deny(self, ctx):
+    msg=ctx.message
+    admin=discord.utils.get(msg.author.guild.roles, name="Server Staff")
+    denied_logs=self.bot.get_channel(ID.fbc.logs.denied) or await self.bot.fetch_channel(ID.fbc.logs.denied)
     if msg.guild is None:
       return
 
@@ -56,30 +58,44 @@ class special(commands.Cog):
       if msg.reference == None:
         return
 
-      welcomed= discord.Object(id=889011345712894002)
-      unwelcomed= discord.Object(id=889011029428801607)
-      admin= discord.utils.get(msg.author.guild.roles, name="Server Staff")
-      reply = msg.reference.resolved
-      member = reply.author
-      gen = self.bot.get_channel(cafe.chat.gen) or await self.bot.fetch_channel(cafe.chat.gen)
-      verify_logs = self.bot.get_channel(ID.fbc.logs.verify) or await self.bot.fetch_channel(ID.fbc.logs.verify)
-      denied_logs = self.bot.get_channel(ID.fbc.logs.denied) or await self.bot.fetch_channel(ID.fbc.logs.denied)
       if admin not in msg.author.roles:
         return
       
-      if msg.content == ".deny":
-        await member.timeout(dt.timedelta(days=7), reason="denied application try again later") 
-        time = await msg.channel.send("time holder(dont delete)")
-        await denied_logs.send(f"\tDenied <@{member.id}>\n Denied id: {member.id}\nDenied:{member}\nDenied by: <@{msg.author.id}>\nDenied by user: {msg.author}\n\nDenied at: {time.created_at}")
-        await msg.channel.delete()
+      member = msg.reference.resolved.author
+      await member.timeout(dt.timedelta(days=7), reason="denied application try again later") 
+      time = await msg.channel.send("time holder(dont delete)")
+      await denied_logs.send(f"\tDenied <@{member.id}>\n Denied id: {member.id}\nDenied:{member}\nDenied by: <@{msg.author.id}>\nDenied by user: {msg.author}\n\nDenied at: {time.created_at}")
+      await msg.channel.delete()
+      
+  @commands.command(hidden=True)
+  async def on_message(self, ctx):
+    msg=ctx.message
+    admin=discord.utils.get(msg.author.guild.roles, name="Server Staff")
+    welcomed=discord.Object(id=889011345712894002)
+    unwelcomed=discord.Object(id=889011029428801607)
+    if msg.guild is None:
+      return
 
-      if msg.content == ".verify":
-        await member.remove_roles(unwelcomed)
-        await member.add_roles(welcomed)
-        if msg.author.id == Fenne:
-          await gen.send(f"Welcome <@{member.id}> please make a <#{cafe.friends.bio}> and enjoy your stay.")
-        else:
-          await gen.send(f"Welcome <@{member.id}> please make a <#{cafe.friends.bio}> and enjoy your stay. Welcomed by <@{msg.author.id}>")
-        time = await msg.channel.send("time holder(dont delete)")
-        await verify_logs.send(f"\tWelcome <@{member.id}>\nWelcome id: {member.id}\nWelcome:{member}\n{reply.content}\n\nWelcomed by: <@{msg.author.id}>\nWelcomer: {msg.author}\n\nWelcomed at: {time.created_at}")
-        await msg.channel.delete()
+    if msg.author.id == botuser:
+      return
+
+    if msg.channel.id != cafe.verify:
+      if msg.channel.category_id != cafe.cats.verify:
+        return
+
+      if msg.reference == None:
+        return
+
+      member = msg.reference.resolved.author
+      if admin not in msg.author.roles:
+        return
+     
+      await member.remove_roles(unwelcomed)
+      await member.add_roles(welcomed)
+      if msg.author.id == Fenne:
+        await gen.send(f"Welcome <@{member.id}> please make a <#{cafe.friends.bio}> and enjoy your stay.")
+      else:
+        await gen.send(f"Welcome <@{member.id}> please make a <#{cafe.friends.bio}> and enjoy your stay. Welcomed by <@{msg.author.id}>")
+      time = await msg.channel.send("time holder(dont delete)")
+      await verify_logs.send(f"\tWelcome <@{member.id}>\nWelcome id: {member.id}\nWelcome:{member}\n{msg.reference.resolved.content}\n\nWelcomed by: <@{msg.author.id}>\nWelcomer: {msg.author}\n\nWelcomed at: {time.created_at}")
+      await msg.channel.delete()
