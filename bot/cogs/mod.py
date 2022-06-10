69ea2883e6a8a63c3c6ea7707fd5e51930cb773e
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-10 19:27:45 -0400

    MORE INDENTS

--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -25,22 +25,22 @@ class MOD(commands.Cog):
     self.bot=bot
   
   @commands.command()
-    async def bio(self, ctx, member: discord.Member=None):
-      """Posts someones bio."""
-      data = await self.bot.bio.find(member.id)
-      if not data:
-        if member == ctx.author:
-          return await ctx.send("You don't have a bio stored.")
-        else:
-          return await ctx.send("They don't have a bio stored.")
+  async def bio(self, ctx, member: discord.Member=None):
+    """Posts someones bio."""
+    data = await self.bot.bio.find(member.id)
+    if not data:
+      if member == ctx.author:
+        return await ctx.send("You don't have a bio stored.")
+      else:
+        return await ctx.send("They don't have a bio stored.")
           
-        bio_channel = self.bot.get_channel(cafe.friends.bio)
-        msg = await bio_channel.fetch_message(data["msg_id"])
+      bio_channel = self.bot.get_channel(cafe.friends.bio)
+      msg = await bio_channel.fetch_message(data["msg_id"])
   
-        view = discord.ui.View()
-        view.add_item(discord.ui.Button(label="Go to bio post", url=msg.jump_url))
+      view = discord.ui.View()
+      view.add_item(discord.ui.Button(label="Go to bio post", url=msg.jump_url))
   
-        await ctx.send(discord.utils.escape_mentions(f"**Bio for {member.name}**\n{data['bio']}"), view=view)
+      await ctx.send(discord.utils.escape_mentions(f"**Bio for {member.name}**\n{data['bio']}"), view=view)
       
   @commands.command()
   async def speak(self, ctx, channel: Optional[discord.TextChannel], member: Optional[discord.Member], *, message: str):
