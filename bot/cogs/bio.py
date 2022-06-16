686b65352c841348927cd384c745c17db2b5613c
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-15 23:35:35 -0400

    bio to non mod

--- bot/cogs/bio.py
+++ bot/cogs/bio.py
@@ -21,3 +21,27 @@ class Bios(commands.Cog):
             "msg_id": msg.id
           }
         )
+        
+  @commands.command()
+  async def bio(self, ctx, member: discord.Member=None):
+    """Posts someones bio."""
+    member = ctx.author if not member else member
+    data = await self.bot.bio.find(member.id)
+    if ctx.channel.id != cafe.chat.bot or ctx.channel.id != cafe.friends.explore or ctx.channel.id == ID.fbc.commands: return
+    guild = self.bot.get_guild(ID.server.cafe) or await self.bot.fetch_guild(ID.server.cafe) 
+    member = guild.get_member(ctx.message.author.id) or await guild.fetch_member(ctx.message.author.id)
+    reg = member.get_role(889011345712894002)
+    if reg not in ctx.author.roles: return
+    if not data:
+      if member == ctx.author:
+        return await ctx.send("You don't have a bio stored.")
+      else:
+        return await ctx.send("They don't have a bio stored.")
+    
+    bio_channel = self.bot.get_channel(cafe.friends.bio)
+    msg = await bio_channel.fetch_message(data["msg_id"])
+  
+    view = discord.ui.View()
+    view.add_item(discord.ui.Button(label="Go to bio post", url=msg.jump_url))
+  
+    await ctx.send(discord.utils.escape_mentions(f"**Bio for {member.name}**\n{data['bio']}"), view=view)
--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -59,26 +59,7 @@ Someone used {ctx.message.content} outside of the cafe, the guild name is {ctx.g
           """
       await logs.send(msg1)
       return False
-    
-      
-  @commands.command()
-  async def bio(self, ctx, member: discord.Member=None):
-    """Posts someones bio."""
-    member = ctx.author if not member else member
-    data = await self.bot.bio.find(member.id)
-    if not data:
-      if member == ctx.author:
-        return await ctx.send("You don't have a bio stored.")
-      else:
-        return await ctx.send("They don't have a bio stored.")
-    
-    bio_channel = self.bot.get_channel(cafe.friends.bio)
-    msg = await bio_channel.fetch_message(data["msg_id"])
-  
-    view = discord.ui.View()
-    view.add_item(discord.ui.Button(label="Go to bio post", url=msg.jump_url))
-  
-    await ctx.send(discord.utils.escape_mentions(f"**Bio for {member.name}**\n{data['bio']}"), view=view)
+
 
   @commands.command(hidden=True)
   async def MODtest(self, ctx):
