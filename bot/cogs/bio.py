2007fe4673456fc52aa13d87239a90be0b4dff18
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-15 23:39:21 -0400

    fix

--- bot/cogs/bio.py
+++ bot/cogs/bio.py
@@ -6,21 +6,21 @@ class Bios(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog.listener()
-    async def on_raw_message_edit(self, payload):
-        if payload.channel_id != cafe.friends.bio:
-            return
+  @commands.Cog.listener()
+  async def on_raw_message_edit(self, payload):
+    if payload.channel_id != cafe.friends.bio:
+      return
 
-        channel = self.bot.get_channel(payload.channel_id)
-        msg = await channel.fetch_message(payload.message_id)
+    channel = self.bot.get_channel(payload.channel_id)
+    msg = await channel.fetch_message(payload.message_id)
 
-        await self.bot.bio.upsert(
-          {
-            "_id": msg.author.id,
-            "bio": str(msg.content),
-            "msg_id": msg.id
-          }
-        )
+    await self.bot.bio.upsert(
+      {
+        "_id": msg.author.id,
+        "bio": str(msg.content),
+         "msg_id": msg.id
+      }
+    )
         
   @commands.command()
   async def bio(self, ctx, member: discord.Member=None):
