fc438ca3771144471cea5fa0060eea09f7bec6b8
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-23 19:08:28 -0400

    update restart command back so it stops the bot

--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -71,6 +71,21 @@ Someone used {ctx.message.content} outside of the cafe, the guild name is {ctx.g
     await ctx.send("workin")
 
   @commands.command(hidden=True)
+  async def restart(self, ctx):
+    have_role = False
+    if ctx.guild.id != ID.server.fbc:
+      return
+    for i in ctx.guild.roles:
+      if i in ctx.author.roles and i.id == 983492505167339670:
+        have_role = True
+    
+    if have_role == False:
+        return
+    if ctx.channel.id != 970411065638780988:
+        return
+    sys.exit()
+    await bot.close()
+  @commands.command(hidden=True)
   async def speak(self, ctx, channel: Optional[discord.TextChannel], member: Optional[discord.Member], *, message: str):
     """Sends a message as the bot. Only works in the busboy-cmds channel.
     :
