cfd69c7dc804229da15319a2586a3ea96779890a
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-06-28 12:51:47 -0400

    plsssss

--- bot/cogs/bingus.py
+++ bot/cogs/bingus.py
@@ -9,11 +9,11 @@ null = None
 botuser = 966392608895152228 
 cctx=commands.Context
 class bingus(commands.Cog):
-  def __init__(self, bot):
-    self.bot=bot
+  def __init__(self, bot: commands.Bot):
+    self.bot: commands.Bot = bot
   
   @commands.hybrid_command(name="bingus")
-  async def bingus(self, ctx: cctx):
+  async def bingus(self, ctx: commands.Context):
     """Posts random bingus image. :p"""
     file=["Z5EW9Ij", "apGRbbd", "bxSmibQ", "0pld30P", "ekv5sS", "cTQxzzz", "Qz0o2au", "0o0YOq9", "Cie89pF", "n4E8Eo7", "a52YHBu", "lYDqcIH", "TZ227yu", "T4RJ0mC", "UqnkEGP", "wAg3rsf", "wSWdCaT", "RqT1tFS", "3VJGRpY", "raerLvq", "LH8VqGH", "xtrV1fj", "GePK3z5", "qOKGQ9p", "dYt8wZk", "qvtC6Ix", "2vgNv4u"]
     files=random.choice(file)
