d76dd2785fdabb865907cc76af861ddffb38d339
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-05 08:51:01 -0400

    fix3

--- bot/cogs/bingus.py
+++ bot/cogs/bingus.py
@@ -15,10 +15,10 @@ class bingus(commands.Cog):
   @commands.hybrid_command(name="bingus")
   async def bingus(self, ctx: commands.Context):
     """Posts random bingus image. :p"""
-    file=["Z5EW9Ij", "apGRbbd", "bxSmibQ", "0pld30P", "ekv5sS", "cTQxzzz", "Qz0o2au", "0o0YOq9", "Cie89pF", "n4E8Eo7", "a52YHBu", "lYDqcIH", "TZ227yu", "T4RJ0mC", "UqnkEGP", "wAg3rsf", "wSWdCaT", "RqT1tFS", "3VJGRpY", "raerLvq", "LH8VqGH", "xtrV1fj", "GePK3z5", "qOKGQ9p", "dYt8wZk", "qvtC6Ix", "2vgNv4u"]
+    file=["Z5EW9Ij", "apGRbbd", "bxSmibQ", "0pld30P", "ekv5sS", "cTQxzzz", "Qz0o2au", "0o0YOq9", "Cie89pF", "n4E8Eo7", "a52YHBu", "lYDqcIH", "TZ227yu", "T4RJ0mC", "UqnkEGP", "wAg3rsf", "wSWdCaT", "RqT1tFS", "3VJGRpY", "raerLvq", "xtrV1fj", "GePK3z5", "qOKGQ9p", "dYt8wZk", "qvtC6Ix", "2vgNv4u"]
     files=random.choice(file)
     embed = discord.Embed()
-    url=f"https://i.imgur.com/{files}"
+    url=f"https://i.imgur.com/{files}.gif"
     print(url)
     embed.set_image(url=url)
     await ctx.send(embed=embed)
