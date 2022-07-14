5d545c2b674c6e46ab803f5190aee6a3f56a357e
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-13 22:33:10 -0400

    248?

--- bot/extras/card_generator.py
+++ bot/extras/card_generator.py
@@ -112,7 +112,7 @@ class Generator:
         length_of_bar = abs((xphave/xpneed) / 747)*100000
         print(str(length_of_bar))
 
-        blank_draw.rectangle((248, 188, length_of_bar, 202), fill=TEXT_CLR)
+        blank_draw.rectangle((248, 188, length_of_bar+248, 202), fill=TEXT_CLR)
         blank_draw.ellipse((20, 20, 218, 218), fill=(255, 255, 255, 0), outline=TEXT_CLR)
 
         profile_pic_holder.paste(profile, (29, 29, 209, 209))
