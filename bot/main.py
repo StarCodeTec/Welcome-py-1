bd3fc34bf8a422cd879e18d89d6de295d3f926b5
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-15 18:18:45 -0400

    connect new command

--- bot/main.py
+++ bot/main.py
@@ -28,6 +28,7 @@ import sys
 sys.path.append('../..')
 from passcodes import main
 sys.path.append('busboy/bot')
+import LIBS.A_change.py as DEBUG_CHANGE
 key=main.fenne.key
 import motor.motor_asyncio
 from db.dbc import client as connection_url
@@ -109,6 +110,7 @@ async def on_guild_channel_create(cha):
 async def main_start(run):
     async with bot:
         purge.start()
+        await bot.add_cog(DEBUG_CHANGE.CHANGE(bot))
         await bot.add_cog(EMOTES.gifs(bot))
         await bot.add_cog(SPECIAL.mod(bot))
         await bot.add_cog(BINGUS_MEME.bingus(bot))
