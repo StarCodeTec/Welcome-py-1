c33f6bb1a715f0b00e67b12017c7dd87c174104c
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-15 11:20:35 -0400

    update the welcome dm

--- bot/extras/text_zone.py
+++ bot/extras/text_zone.py
@@ -1,12 +1,14 @@
 class BIG:
     welcome_text_dm = """to the Femboy Cafe!
-
-✧ₓ˚.✧ This server is owned and operated by Fenne Wolf
-✧ₓ˚.✧ Join me on Social Media!
-✧ₓ˚.✧ https://www.linktr.ee/littlefenne
-
-⇀ Be sure to check in at the <#945087125831958588>  to verify for full access.
-⇀ Share our server link discord.gg/femboycafe
+    
+    ✧ This server is owned and operated by Fenne Wolf
+    ✧ Support the server on Patreon
+    ✧ₓ˚.✧ https://www.patreon.com/littlefenne
+    ✧ Join me on Social Media!
+    ✧ₓ˚.✧ https://www.linktr.ee/littlefenne
+    
+    ⇀ Be sure to verify here <#945087125831958588> for full access.
+    ⇀ Share our server link discord.gg/femboycafe 
 """
     wt = "to the server. Feel free to make a <#888482614351134720> and enjoy your stay!"
 
