e59dce8249a26e396462bb31b0f49e249bf15344
jagg3127 <jhaught2007@gmail.com>
2022-07-15 17:41:04 -0400

    MESSAGE UPDATES

--- bot/LIBS/git_clone.sh
+++ bot/LIBS/git_clone.sh
@@ -2,4 +2,4 @@
 cd /home/dev/busboy
 git add --all .
 git commit -m "MESSAGE UPDATES"
-git push https://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@github.com/jagg3127-alt/Welcome-py-1 main
+git push https://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@github.com/jagg3127/Welcome-py-1 main
