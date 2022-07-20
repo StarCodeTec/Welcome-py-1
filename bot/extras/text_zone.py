fa374ef96e524a41f90520ba526aba0e17be5d64
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-20 12:33:37 -0400

    fix return statement

--- bot/extras/text_zone.py
+++ bot/extras/text_zone.py
@@ -4,7 +4,7 @@ sys.path.append('/home/dev/busboy/bot/LIBS')
 os.chdir('/home/dev/busboy/bot/LIBS')
 import A_LIB_change as CHANGE
 def read(c):
-    CHANGE.main(c, "r", None)
+    return CHANGE.main(c, "r", None)
 class BIG:
     wd=read("wd")
     wt=read("wt")
