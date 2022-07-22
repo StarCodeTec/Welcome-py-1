90d2c6cec7da672bbc4c1836a0d4577600851519
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-21 22:19:38 -0400

    pasted the wrong code oof

--- bot/extras/text_zone.py
+++ bot/extras/text_zone.py
@@ -1,52 +1,18 @@
-import json
 import os
 import sys
-FILE_PATH='/home/dev/busboy/bot/LIBS/'
+#FILE_PATH='/home/dev/busboy/bot/LIBS'
+FILE_PATH='/storage/emulated/0/qpython/projects3/JSON/LIBS'
 sys.path.append(FILE_PATH)
 os.chdir(FILE_PATH)
-def WRITE_SYSTEM(placement, array, TEXT, data, cn):
-  add=[]
-  for x in range(len(data)):
-    wrong_array=data[x]
-    if cn != wrong_array["name"]:
-      add.append(wrong_array)
-    else:
-      add.append({'name': cn, 'id': TEXT})
-  MAIN = {'data': add}
-  print(MAIN)
-  with open(f'{FILE_PATH}JSON/file.json', 'w') as f:
-    json.dump(MAIN, f)
-    f.close() 
-  #return add
-  
-  
-
-def write(command_name, TEXT):
-  cn=command_name
-  with open(f'{FILE_PATH}JSON/file.json', 'r') as f: 
-    data = json.load(f)
-    data=data["data"]
-    f.close()
-  for x in range(len(data)):
-    array=data[x]
-    if cn == array["name"]:
-      return WRITE_SYSTEM(x, array, TEXT, data, cn)
-  
-    
-    
-def read(FIND):
-  with open(f'{FILE_PATH}LIBS/JSON/test.json', 'r') as f: 
-    data = json.load(f)
-    data=data["data"]
-    f.close()
-  ids = [] 
-  for piece in data:
-    name = piece['name'] 
-    ID = piece['id']
-    ids.append(name)
-    ids.append(ID)    
-
-  for x in range(len(ids)):
-    if ids[x] == FIND:
-      return ids[x+1]
-
+import A_LIB_change as CHANGE
+def read(read): CHANGE.read(read)
+class BIG:
+    wd=read("wd")
+    wt=read("wt")
+    bt=read("bio")
+class all_id:
+    fenne = "<:little_fenne:925500399656509489>"
+    check = "<:check:919007866940182589>"
+    heart = "❤"
+    P_heart = "💜"
+    thumb_up = "👍"
