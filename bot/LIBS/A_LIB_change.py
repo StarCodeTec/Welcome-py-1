db9bc916d0292f420c8f0a808b7a58040e642b5c
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-21 22:17:55 -0400

    update library

--- bot/LIBS/A_LIB_change.py
+++ bot/LIBS/A_LIB_change.py
@@ -1,17 +1,46 @@
+import json
 import os
 import sys
-import json 
-sys.path.append('/home/dev/busboy/bot/LIBS')
-os.chdir('/home/dev/busboy/bot/LIBS')
-array=[]
-LOCAL=globals()
-def FIND_DATA(FIND):
-  with open('JSON/file.json', 'r') as f: 
+FILE_PATH='/home/dev/busboy/bot/LIBS/'
+sys.path.append(FILE_PATH)
+os.chdir(FILE_PATH)
+
+def WRITE_SYSTEM(placement, array, TEXT, data, cn):
+  add=[]
+  for x in range(len(data)):
+    wrong_array=data[x]
+    if cn != wrong_array["name"]:
+      add.append(wrong_array)
+    else:
+      add.append({'name': cn, 'id': TEXT})
+  MAIN = {'data': add}
+  print(MAIN)
+  with open(f'{FILE_PATH}JSON/file.json', 'w') as f:
+    json.dump(MAIN, f)
+    f.close() 
+  #return add
+  
+  
+
+def write(command_name, TEXT):
+  cn=command_name
+  with open(f'{FILE_PATH}JSON/file.json', 'r') as f: 
     data = json.load(f)
     data=data["data"]
-    LOCAL["datax"]=data
     f.close()
-  LOCAL["ids"] = [] 
+  for x in range(len(data)):
+    array=data[x]
+    if cn == array["name"]:
+      return WRITE_SYSTEM(x, array, TEXT, data, cn)
+  
+    
+    
+def read(FIND):
+  with open(f'{FILE_PATH}LIBS/JSON/test.json', 'r') as f: 
+    data = json.load(f)
+    data=data["data"]
+    f.close()
+  ids = [] 
   for piece in data:
     name = piece['name'] 
     ID = piece['id']
@@ -20,36 +49,5 @@ def FIND_DATA(FIND):
 
   for x in range(len(ids)):
     if ids[x] == FIND:
-      return x+1
+      return ids[x+1]
 
-def WRITE_SYSTEM(z, b):
-  key=ids[z-1]
-  data=datax.get(key)
-  print(data)
-  return
-    
-  MAIN = {'data': [text]}
-  print(MAIN)
-  f=open('JSON/file.json', 'w')
-  json.dump(MAIN, f)
-  f.close() 
-
-def main(start_core, MAIN_CORE, TEXT):
-  if MAIN_CORE == "w": 
-    MAIN_CORE=False
-  elif MAIN_CORE == "r":
-    MAIN_CORE=True
-  if MAIN_CORE:
-    x=FIND_DATA(start_core)
-    return ids[x]
-  aaax=FIND_DATA(start_core)
-  return WRITE_SYSTEM(aaax, TEXT)
-
-
-
-
-"""
-start_core=command
-main_core=r/w
-text=text
-"""
