eb4b00ffe2cb66473f81ad17b97ea2d70cf31d21
jhaught2007@gmail.com <jhaught2007@gmail.com>
2022-07-30 09:52:53 -0400

    chang MOD to mod

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -28,7 +28,7 @@ class BusboyHelp(commands.HelpCommand):
                 if cog and cog.description:
                     value = '{}'.format(value)
             
-                if name != "MOD":
+                if name != "mod":
                     embed.add_field(name=f"__{name}__", value=f'{value}', inline=False)
         
         await self.get_destination().send(embed=embed)
