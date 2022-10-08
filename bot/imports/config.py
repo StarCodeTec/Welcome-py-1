55ac070bed3fa36c081a1b38af2070bc8aa476f9
jagg3127 <jhaught2007@gmail.com>
2022-10-08 13:33:34 -0400

    config broke

--- bot/imports/config.py
+++ bot/imports/config.py
@@ -11,7 +11,7 @@ intents      = discord.Intents.all()
 ACTIVITY     = discord.Activity(type=discord.ActivityType.watching, name="discord.gg/FemboyCafe")
 bot          = commands.Bot(command_prefix=commands.when_mentioned_or('.', '. '), intents=intents, activity=ACTIVITY, case_insensitive=True, tree_cls=app_commands.CommandTree)
 
-bot.dpg      = Database(f'postgresql://postgres:{main.POST_DB}@127.0.0.1:5434/pybot_pg')
+bot.dpg      = Database(f'postgresql://postgres:{main.POST_DB}@127.0.0.1:5432/pybot_pg')
 bot.pg       = pg
 
 bot.mongo    = motor.motor_asyncio.AsyncIOMotorClient(str(connection_url))
