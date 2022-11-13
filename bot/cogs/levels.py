efd06095e454dc368a54c647d450dd9c012aefc7
jagg3127 <jhaught2007@gmail.com>
2022-11-13 15:03:01 -0500

    update

--- bot/cogs/levels.py
+++ bot/cogs/levels.py
@@ -310,6 +310,7 @@ class Levels(commands.Cog):
         out   = []
         place = 1
         for item in data:
+            print(item)
             member = ctx.guild.get_member(item["_id"])
             xp     = "{:,}".format(item['xp'])
             if not member:
