b51013dfc02db4802bf79000593c15f8bdf85433
jagg3127 <jhaught2007@gmail.com>
2022-11-13 15:37:19 -0500

    finsished fixing bug

--- bot/cogs/levels.py
+++ bot/cogs/levels.py
@@ -310,7 +310,7 @@ class Levels(commands.Cog):
         out   = []
         place = 1
         for item in data:
-            member = ctx.guild.get_member(item["_id"]) or  ctx.guild.fetch_member(item["_id"])
+            member = ctx.guild.get_member(item["_id"])
             xp     = "".format(item['xp'])
             if not member:
                 return await self.bot.dpg.execute(query=f"DELETE FROM levels WHERE _id = {item['_id']}")
